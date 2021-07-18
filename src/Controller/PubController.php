26e62f8c0df2404651058e80b123fc9dd8cfe9dd
Zied Neji <zied.neji@esprit.tn>
2021-07-18 21:51:20 +0100

    pucblicité apis

--- src/Controller/PubController.php
+++ src/Controller/PubController.php
@@ -184,7 +184,7 @@ class PubController extends AbstractController
     }
     
     /**
-     * @Route("/api/publicites/affichagePubCinema/{id}", name="affichagePubCinema")
+     * @Route("/api/publicites/cinema/{id}", name="affichagePubCinema")
      */
     public function affichageCinemaPub( Request $request , $id, PubliciteRepository $rep
         ,  \Swift_Mailer $mailer
